a90f0711cbe26f0d8113116e39aa2ddfd647e32d
canhcd14 <91421049+canhcd14@users.noreply.github.com>
2021-09-26 21:33:08 +0700

    Add files via upload

---
+++ avod/avod/builders/bev_generator_builder.py
@@ -0,0 +1,13 @@
+from avod.core.bev_generators import bev_slices
+
+import sys
+sys.path.append('F:\\avod_pl\\')
+def build(bev_maps_type_config, kitti_utils):
+
+    bev_maps_type = bev_maps_type_config.WhichOneof('bev_maps_type')
+
+    if bev_maps_type == 'slices':
+        return bev_slices.BevSlices(
+            bev_maps_type_config.slices, kitti_utils)
+
+    raise ValueError('Invalid bev_maps_type', bev_maps_type)
