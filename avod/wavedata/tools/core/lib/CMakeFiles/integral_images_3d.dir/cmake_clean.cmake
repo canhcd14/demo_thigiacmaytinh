acd310a9c41e54835e824632567621b65e61dcfb
canhcd14 <91421049+canhcd14@users.noreply.github.com>
2021-09-26 21:30:52 +0700

    Add files via upload

---
+++ avod/wavedata/tools/core/lib/CMakeFiles/integral_images_3d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/integral_images_3d.dir/integral_images_3d.cpp.o"
+  "libintegral_images_3d.pdb"
+  "libintegral_images_3d.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/integral_images_3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
